b4123e1c948e2b5cc8ca3151a437aefc14898802
Temitope Balogun <tsbalogun97@yahoo.com>
2023-06-26 22:50:00 -0500

    edited verison

--- app.js
+++ app.js
@@ -31,12 +31,43 @@ let majorContainer ={
             textDescription: 'Update your everyday wardrobe with our collection of men’s pants. Discover tailored suit pants for work or special occasions, as well as comfier sweatpants and joggers for downtime. Mix up your weekend look and swap the jeans for a pair of cargo pants or chinos. Browse a range of colors, from neutral blacks and grays to brighter shades and prints. Find everything from soft breathable cotton to functional track pants and luxe velvet dress pants.',
             lowImage: 'https://images.unsplash.com/photo-1516826957135-700dedea698c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
         },
-        womens: {},
-        kids: {}
+        womens: {
+            headImage: "https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29tZW4lMjBjbG90aGluZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+            optionTags: ["Blazers", "Knitwears", "Blouses", "Sweatshirts"],
+            productImages: [
+                {
+                    name: 'Blazers',
+                    pic: "https://static.zara.net/photos///2023/I/0/1/p/7590/016/330/2/w/602/7590016330_1_1_1.jpg?ts=1684930649193s"
+                },
+                {
+                    name: 'Knitwears',
+                    pic: "https://static.zara.net/photos///2023/V/0/1/p/3920/017/303/2/w/602/3920017303_2_2_1.jpg?ts=1681463034658"
+                },
+                {
+                    name: 'Blouses',
+                    pic: "https://static.zara.net/photos///2023/V/0/1/p/3920/017/303/2/w/602/3920017303_2_2_1.jpg?ts=1681463034658"
+                },
+                {
+                    name: 'Sweatshirts',
+                    pic: "https://static.zara.net/photos///2022/I/0/1/p/5039/739/743/17/w/359/5039739743_2_1_1.jpg?ts=1670255445041"
+                },
+                {
+                    name: 'Jackets',
+                    pic: "https://static.zara.net/photos///2023/V/0/1/p/2583/872/800/2/w/602/2583872800_6_30_1.jpg?ts=1675328617871"
+                },
+                {
+                    name: 'Shoes',
+                    pic: "https://static.zara.net/photos///2023/V/1/1/p/2276/110/500/2/w/359/2276110500_6_1_1.jpg?ts=1676299948496"
+                },
+            ],
+            textDescription: 'Update your everyday wardrobe with our collection of men’s pants. Discover tailored suit pants for work or special occasions, as well as comfier sweatpants and joggers for downtime. Mix up your weekend look and swap the jeans for a pair of cargo pants or chinos. Browse a range of colors, from neutral blacks and grays to brighter shades and prints. Find everything from soft breathable cotton to functional track pants and luxe velvet dress pants.',
+            lowImage: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29tZW4lMjBtb2RlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60'
+        },
+        
     }
 // -----------------------
 
-let mainImage = document.querySelector('.mainImg')
+let mainImage = document.querySelector('.topPic')
 let option = document.querySelector('.options')
 let miniOp = document.querySelectorAll('.choice')
 let productBox = document.querySelector('.productContainer')
@@ -47,18 +78,24 @@ const changeAll = () => {
     changeTopPic()
     changeOptions()
     product()
+    lowImage()
+    textContainer()
 }
     const changeTopPic = () =>{
+        mainImage.src = majorContainer.mens.headImage
+        
+        
         // createImage 
-        let newimage = document.createElement('img')
-            newimage.setAttribute('src', majorContainer.mens.headImage)
-            newimage.setAttribute('class','topPic')
-        // replaceWith current
-        mainImage.replaceWith(newimage)
+        // let newimage = document.createElement('img')
+        //     newimage.setAttribute('src', majorContainer.mens.headImage)
+        //     newimage.setAttribute('class','topPic')
+        // // replaceWith current
+        // mainImage.replaceWith(newimage)
 
     }
 
     const changeOptions = () =>{
+        option.innerHTML = ''
         majorContainer.mens.optionTags.forEach((tagname)=>{
             // CREATE ELE
             let txt = document.createElement('div')
@@ -69,6 +106,7 @@ const changeAll = () => {
     }
 
     const product= () => {
+        productBox.innerHTML = ''
         majorContainer.mens.productImages.forEach((obj)=>{
             let shopBox  = document.createElement('img')
             shopBox.setAttribute('src',obj.pic)
@@ -77,4 +115,67 @@ const changeAll = () => {
         })
 
     }
+// placing the lowImage inside the polaroid pic
 
+
+    const lowImage = () => {
+        pic.src = majorContainer.mens.lowImage
+    }
+
+    // place text inside the textContainer
+    const textContainer = () => {
+        textBox.innerHTML = majorContainer.mens.textDescription
+    }
+
+// Women's Function
+const changeAllWomens = () => {
+    changeWomensTopPic()
+    changeWomensOptions()
+    womensProduct()
+    womensLowImage()
+    womensTextContainer()
+}
+    const changeWomensTopPic = () =>{
+        mainImage.src = majorContainer.womens.headImage
+        
+        // // createImage 
+        // let newimage = document.createElement('img')
+        //     newimage.setAttribute('src', majorContainer.womens.headImage)
+        //     newimage.setAttribute('class','topPic')
+        // // replaceWith current
+        // mainImage.replaceWith(newimage)
+
+    }
+
+    const changeWomensOptions = () =>{
+        option.innerHTML = ''
+        majorContainer.womens.optionTags.forEach((tagname)=>{
+            // CREATE ELE
+            let txt = document.createElement('div')
+            txt.innerHTML = tagname
+            // REPLACE ELEMENT WITH NEW ELEMENT
+            option.append(txt)
+        })
+    }
+
+    const womensProduct= () => {
+    //    empting the productBox
+        productBox.innerHTML = ''
+        majorContainer.womens.productImages.forEach((obj)=>{
+            let shopBox  = document.createElement('img')
+            shopBox.setAttribute('src',obj.pic)
+            shopBox.setAttribute('class','product')
+            productBox.append(shopBox)
+        })
+
+    }
+// placing the womensLowImage inside the polaroid pic
+
+    const womensLowImage = () => {
+        pic.src = majorContainer.womens.lowImage
+    }
+
+    // place text inside the womensTextContainer
+    const womensTextContainer= () => {
+        textBox.innerHTML = majorContainer.womens.textDescription
+    }
